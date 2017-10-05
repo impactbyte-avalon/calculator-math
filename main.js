0e45953bbd33b52a9810e98e5abdf99a4f6dfa2f
hafidz <hafidzzpermana09@gmail.com>
2017-10-05 11:20:53 +0700

    Add code comment and cases

---
+++ main.js
@@ -0,0 +1,33 @@
+/************** LEVEL 0 ***************/
+/Prepared
+/************** LEVEL 1 ***************/
+function add(numberA, numberB) {
+	return numberA + numberB;
+}
+console.log(add(9, 4));
+
+
+function substact(numberC, numberD) {
+	return numberC - numberD;
+}
+console.log(substact(90, 20));
+
+
+function multiply(numberE, numberF) {
+	return numberE * numberF;
+}
+console.log(multiply(6, 2));
+
+
+function divide(numberG, numberH) {
+	return numberG / numberH;
+}
+console.log(divide(9, 3));
+
+
+
+/************** LEVEL 2 ***************/
+function nested(add,multiply) {
+	return add * multiply;
+}
+console.log(nested());
