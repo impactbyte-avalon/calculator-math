f328724436300069bd447668c0270a1ff488d1a4
Andrew <sbstn.andrew56@gmail.com>
2017-10-05 15:04:18 +0700

    calculator math

---
+++ index.js
@@ -0,0 +1,17 @@
+function add(x, y) {
+    return x + y;
+}
+
+function substract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+console.log(substract(7,3)+multiply(20,4)-divide(50,2))
