77ce15943a4b1d4ea3467f2742e4ae2ad47007b0
Arian <arianmarkus77@yahoo.com>
2017-10-05 15:33:45 +0700

    Calculator Math

---
+++ index.js
@@ -0,0 +1,41 @@
+
+function add(x,y) {
+  const z = x + y ;
+  return z
+}
+
+
+console.log(add(200,50));
+
+function substract(x,y) {
+  const z = x - y ;
+  return z
+}
+
+console.log(substract(200,50));
+
+function multiply(x,y) {
+  const z = x * y ;
+  return z
+}
+
+console.log(multiply(200,50));
+
+function divide(x,y) {
+  const z = x / y ;
+  return z
+}
+
+console.log(divide(200,50));
+
+function modul(x,y) {
+  const z = x % y ;
+  return z
+}
+
+console.log(modul(200,50));
+
+
+const result = add(substract(3,4), 9999)
+
+console.log(result);
