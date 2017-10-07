c4be7c8c6ba2dc7173965fa96258d4c72e81f8c0
rian <adrianpradana301@gmail.com>
2017-10-07 13:59:36 +0700

    Adrian assignment

---
+++ index.js
@@ -0,0 +1,28 @@
+//assignment for level 0-1
+function add (a+b){
+    hasil = a+b;
+    return hasil;
+}
+
+function substract(c,d){
+    hasil = c-d;
+    return hasil;
+}
+
+function multiply(e,f){
+    hasil = e*f;
+    return hasil;
+}
+
+function divide(k,l){
+    hasil = k/l;
+    return hasil;
+}
+
+function modulo(x,y){
+    hasil = x%y;
+    return hasil;
+}
+
+modulo(15,7)
+console.log(hasil)
